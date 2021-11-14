f59f1b26f2d86cd5522e1fd9c332271160cc0c7f
GGorAA <ggoraa1029@gmail.com>
2021-11-14 18:11:00 +0200

    Update kumitateru.rb

--- Formula/kumitateru.rb
+++ Formula/kumitateru.rb
@@ -1,7 +1,7 @@
 class Kumitateru < Formula
   desc "Build system for Garmin ConnectIQ. Simple, fast, powerful!"
   homepage "https://github.com/ggoraa/kumitateru"
-  url "https://github.com/ggoraa/kumitateru/releases/download/0.4.0/kumitateru-mac"
+  url "https://github.com/ggoraa/kumitateru/archive/refs/tags/0.4.0.zip"
   sha256 "a286ad0d3f729464294fa23c85cc40df04d6c8f4c28ca6015dea387f1f84362d"
   license "GPL-3.0"
 
