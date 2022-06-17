67477bae082400ef64d8af6cdece000fce348fe7
GGorAA <ggoraa1029@gmail.com>
2022-06-18 00:25:58 +0300

    Update moc.rb

--- Casks/moc.rb
+++ Casks/moc.rb
@@ -1,6 +1,7 @@
 cask "moc" do
-  version "0.1.0"
-  sha256 "8272ee4dd2595cd984842912a12f6ca9274e416a3194b6c1ca1f60a8d95f208d"
+  version "0.2.0"
+  sha256 "c7b8aba065d63885f3f71e0dcb200565ce135cf52fd9b75d4e6438edab4c57cb"
+  revision 1
 
   url "https://github.com/ggoraa/moc/releases/download/#{version}/Moc.dmg"
   name "Moc"
