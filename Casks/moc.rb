f1ed5f01b438eccdce6e6a9ac1d39bc2046b8de1
GGorAA <ggoraa1029@gmail.com>
2022-08-02 16:42:31 +0300

    Set minimum macOS version for Moc to Monterey

--- Casks/moc.rb
+++ Casks/moc.rb
@@ -1,6 +1,7 @@
 cask "moc" do
   version "0.2.0-p2"
   sha256 "1f95c04d9b00f5adf7b9abc202136f1da5b3e33ce3a9665f5c84881ef05f0d48"
+  depends_on macos: ">= :monterey"
 
   url "https://github.com/mock-foundation/moc/releases/download/#{version}/Moc.dmg"
   name "Moc"
